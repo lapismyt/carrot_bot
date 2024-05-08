7954137daa97f4bb0e57f81e227ea03cfc29ce41
LapisMYT <nikitagavrilin005@gmail.com>
2024-05-08 16:48:09 +0300

    .

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiofiles import os, open
+from aiofiles import open
+from aiofiles import os as aioos
 from utils import MarkovChat
 import asyncio
 import random
+import os
 
 bot = Bot(os.getenv('CARROT_BOT_TOKEN'))
 dp = Dispatcher()
 
 @dp.message()
 async def handle_message(message: types.Message):
-    datasets_list = await os.listdir('datasets')
-    if not f'{message.chat.id}.txt' in datasets_list:
-        async with open(f'datasets/{message.chat.id}.txt', 'w') as f:
+    datasets_list = await aioos.listdir('datasets')
+    if message.text == '/start':
+        return None
+    if not f'chat_{message.chat.id}.txt' in datasets_list:
+        async with open(f'datasets/chat_{message.chat.id}.txt', 'w') as f:
             await f.write(f'{message.text} <|end|>\n')
         async with open('datasets/global_dataset.txt', 'a') as f:
             await f.write(f'{message.text} <|end|>\n')
     else:
-        async with open(f'datasets/{message.chat.id}.txt', 'a+') as f:
+        async with open(f'datasets/chat_{message.chat.id}.txt', 'a+') as f:
             await f.write(f'{message.text} <|end|>\n')
         async with open('datasets/global_dataset.txt', 'a') as f:
             await f.write(f'{message.text} <|end|>\n')
         if '@carrot_chatbot' in message.text.lower() or random.randint(0, 10) == 5:
             chain = MarkovChat()
             await chain.train('datasets/global_dataset.txt', weight=1)
-            await chain.train(f'datasets/{message.chat.id}.txt', weight=5)
+            await chain.train(f'datasets/chat_{message.chat.id}.txt', weight=5)
             response = await chain.generate_response(message.text)
-            async with open(f'datasets/{message.chat.id}.txt', 'a+') as f:
+            async with open(f'datasets/chat_{message.chat.id}.txt', 'a+') as f:
                 await f.write(f'{response} <|end|>\n')
             async with open('datasets/global_dataset.txt', 'a') as f:
                 await f.write(f'{response} <|end|>\n')
