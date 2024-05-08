e18081e74109872162fb46d17e1a18d725b44be9
Nikita Gavrilin <nikitagavrilin005@gmail.com>
2024-05-08 21:45:40 +0300

    .

--- main.py
+++ main.py
@@ -1,51 +1,52 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from aiofiles import open
-from aiofiles import os as aioos
-import asyncio
-import random
 import os
+import random
 import markovify
+from telebot import TeleBot, types
 
-bot = Bot(os.getenv('CARROT_BOT_TOKEN'))
-dp = Dispatcher()
+bot = TeleBot(os.getenv('CARROT_BOT_TOKEN'))
 
-@dp.message()
-async def handle_message(message: types.Message):
-    datasets_list = await aioos.listdir('datasets')
-    if message.text is None or message.text.startswith('/') or message.text.lower() == '@carrot_chatbot':
-        return None
-    if not f'chat_{message.chat.id}.txt' in datasets_list:
-        async with open(f'datasets/chat_{message.chat.id}.txt', 'w') as f:
-            await f.write(f'{message.text.lower().replace("@carrot_chatbot", "")}\n')
-        async with open('datasets/global_dataset.txt', 'a') as f:
-            await f.write(f'{message.text.lower().replace("@carrot_chatbot", "")}\n')
+global_model = None
+if os.path.exists('datasets/global.txt'):
+    with open('datasets/global.txt', 'r') as f:
+        global_model = markovify.Text(f.read())
+
+def get_chat_model(chat_id):
+    chat_file = f'datasets/chat_{chat_id}.txt'
+    if os.path.exists(chat_file):
+        with open(chat_file, 'r') as f:
+            chat_model = markovify.Text(f.read())
+        if global_model:
+            chat_model = markovify.combine([chat_model, global_model], [1, 0.5])
+        return chat_model
     else:
-        async with open(f'datasets/chat_{message.chat.id}.txt', 'a+') as f:
-            await f.write(f'{message.text.lower().replace("@carrot_chatbot", "")}\n')
-        async with open('datasets/global_dataset.txt', 'a') as f:
-            await f.write(f'{message.text.lower().replace("@carrot_chatbot", "")}\n')
-        if '@carrot_chatbot' in message.text.lower() or random.randint(2, 10) == 5:
-            async with open(f'datasets/chat_{message.chat.id}.txt') as f:
-                chain = markovify.NewlineText(await f.read())
-                query = message.text.lower().replace('@carrot_chatbot', '')
-                response = await chain.make_short_sentence(150)
-            if not response:
-                print("Empty")
-                return None
-            async with open(f'datasets/chat_{message.chat.id}.txt', 'a+') as f:
-                lines = len((await f.read()).split('\n'))
-                if lines >= 150:
-                    await f.write(f'{response}\n')
-            async with open('datasets/global_dataset.txt', 'a+') as f:
-                lines = len((await f.read()).split('\n'))
-                if lines >= 150:
-                    await f.write(f'{response}\n')
-            await message.answer(response)
-        
+        if global_model:
+            return global_model
+        else:
+            return None
 
-async def main():
-    await dp.start_polling(bot)
+def generate_text(chat_id):
+    chat_model = get_chat_model(chat_id)
+    if chat_model:
+        return chat_model.make_short_sentence(140)
+    else:
+        return None
+        
+@bot.message_handler(content_types=['text'])
+def message_handler(message: types.Message):
+    chat_id = message.chat.id
+    message_text = message.text
+
+    chat_file = f'datasets/chat_{chat_id}.txt'
+    with open(chat_file, 'a') as f:
+        f.write(message_text + '\n')
+
+    with open('datasets/global.txt', 'a') as f:
+        f.write(message_text + '\n')
+
+    if random.randint(1, 6) == 1 or '@carrot_chatbot' in message_text.lower():
+        generated_text = generate_text(chat_id)
+        if generated_text:
+            bot.send_message(chat_id, generated_text)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    bot.infinity_polling()
